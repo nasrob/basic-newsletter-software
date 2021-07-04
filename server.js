3d765e1c473c2acfb88a993da97ddf17543d3a47
Nasser BOUKEHIL <boukehil.nasreddine@gmail.com>
2021-07-04 18:29:24 +0200

    added auth feature

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ require('dotenv').config()
 const express = require('express')
 const { check } = require('express-validator')
 const Airtable = require('airtable')
+const fs = require('fs')
+const { createJWT, verifyJWT } = require('./auth')
+const cookieParser = require('cookie-parser')
 const app = express()
 
 Airtable.configure({
@@ -14,6 +17,7 @@ const table = base(process.env.AIRTABLE_TABLE_NAME)
 
 app.use(express.json())
 app.use(express.static('public'))
+app.use(cookieParser())
 
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/views/index.html')
@@ -40,6 +44,27 @@ app.post('/form', [
     })
 
     res.end()
+})
+
+const initializedFile = './.data/initialized'
+
+app.get('/admin', (req, res) => {
+    if (fs.existsSync(initializedFile)) {
+        // admin auth already initialized
+        verifyJWT(req.cookies.token).then(decodedToken => {
+            res.sendFile(__dirname + '/views/admin.html')
+        }).catch(err => res.status(400).json({ message: 'Invalid auth token provided.'}))
+    } else {
+        // admin auth not initialized
+        const token = createJWT({
+            maxAge: 60 * 24 * 365 // 1 year 
+        })
+
+        fs.closeSync(fs.openSync('./.data/initialized', 'w'))
+
+        res.cookie('token', token, { httpOnly: true, /*secure: true*/ })
+        res.sendFile(__dirname + '/views/admin.html')
+    }
 })
 
 app.listen(3000, () => console.log('Server Ready'))
